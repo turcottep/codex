7a4a308fab096e19546d61b315e910e4de2e2c05
isALEXme <alexis.tremblay@hotmail.ca>
2020-11-24 18:25:42 -0500

    Removed Hidden_frame, now works on Safari

--- codex-app/pages/components/NameForm.jsx
+++ codex-app/pages/components/NameForm.jsx
@@ -2,81 +2,89 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 
 export default class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
+    constructor(props) {
+        super(props);
+        this.state = { value: "" };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit() {
-    submitted = true;
-    $("#gform *").fadeOut(500);
-    $("#gform").prepend(
-      "<h1>Thanks For Signing Up, We'll Keep You Posted!</h1>"
-    );
-  }
-
-  loadFrame() {
-    if (submitted) {
-      window.open = function (url, name, features, replace) {
-        $('#hidden_iframe').attr('src', url);
+        this.postToGoogle = this.postToGoogle.bind(this);
     }
-    }
-  }
 
-  render() {
-    return (
-      <div>
-        <form
-          className={styles.form}
-          name="gform"
-          id="gform"
-          action="https://www.docs.google.com/forms/d/1qJ8yOg670AI2dsUweVNgeSpyVcBIdoOYdN14VpRt8ko/formResponse?"
-          target="hidden_iframe"
-          onSubmit={this.handleSubmit}
-        >
-          <label for="fieldStudy">What's your field of Study?</label>
-          <br></br>
-          <select
-            className={styles.input}
-            id="entry.1812034651"
-            name="entry.1812034651"
-            class="button"
-          >
-            <option selected value="default"></option>
-            <option value="engineering">Engineering</option>
-            <option value="sciences">
-              Sciences (Computer, Pure, Human, etc.)
-            </option>
-            <option value="law">Law</option>
-            <option value="teaching">Teaching</option>
-            <option value="philosophy">Philosophy</option>
-            <option value="marketing">Marketing</option>
-            <option value="administration">Administration</option>
-            <option value="mathematics">Mathematics</option>
-            <option value="other">Other</option>
-          </select>{" "}
-          <br></br> <br></br>
-          <input
-            type="email"
-            className={styles.email}
-            name="entry.217945379"
-            id="entry.217945379"
-            placeholder="Your email"
-            required
-          />
-          <button className={styles.submit} type="submit" id="submit_button">
-            Sign Up
+    postToGoogle() {
+        console.log("lol")
+        var field1 = $("#email").val(); 
+        var field2 = $("#field_study").val();
+
+        $.ajax({
+            url: "https://www.docs.google.com/forms/d/1qJ8yOg670AI2dsUweVNgeSpyVcBIdoOYdN14VpRt8ko/formResponse?",
+            data: {
+                "entry.217945379": field1,
+                "entry.1812034651": field2
+            },
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function () {
+                    //Success message
+                },
+                200: function () {
+                    //Success Message
+                }
+            }
+        });
+
+        $("#gform *").fadeOut(500);
+        $("#gform").prepend(
+            "<h1>Thanks For Signing Up, We'll Keep You Posted!</h1>"
+        );
+
+    }
+
+    render() {
+        return (
+            <div name="gform"
+                id="gform">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+                <form id="form" className={styles.form} 
+                >
+
+                    <label for="fieldStudy">What's your field of Study?</label>
+                    <br></br>
+                    <select
+                        className={styles.input}
+                        id="field_study"
+                        name="entry.1812034651"
+                        class="button"
+                    >
+                        <option selected value="default"></option>
+                        <option value="engineering">Engineering</option>
+                        <option value="sciences">Sciences (Computer, Pure, Human, etc.)</option>
+                        <option value="law">Law</option>
+                        <option value="teaching">Teaching</option>
+                        <option value="philosophy">Philosophy</option>
+                        <option value="marketing">Marketing</option>
+                        <option value="administration">Administration</option>
+                        <option value="mathematics">Mathematics</option>
+                        <option value="other">Other</option>
+                    </select>{" "}
+                    <br></br> <br></br>
+
+                    <input
+                        type="email"
+                        className={styles.email}
+                        name="entry.217945379"
+                        id="email"
+                        placeholder="Your email"
+                        required
+                    />
+                <button type="button" className={styles.submit} onClick={this.postToGoogle}>
+                        Sign Up
           </button>
-        </form>
-        <iframe
-          className={styles.hidden}
-          id="hidden_iframe"
-          name="hidden_iframe"
-          onload="loadFrame()"
-        ></iframe>
-      </div>
-    );
-  }
+                </form>
+
+            </div>
+        );
+    }
 }
+
+
