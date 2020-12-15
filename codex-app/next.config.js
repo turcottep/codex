13e68b8e346940ac0105926c053d53e18ccfe65c
Philippe Turcotte <phil0rdi@hotmail.fr>
2020-12-15 14:14:45 -0500

    Made the app a PWA

--- codex-app/next.config.js
+++ codex-app/next.config.js
@@ -1,5 +1,5 @@
 const withPWA = require('next-pwa')
- 
+
 module.exports = withPWA({
     pwa: {
         dest: 'public'
--- codex-app/pages/index.js
+++ codex-app/pages/index.js
@@ -1,25 +1,70 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Home2 from "./components/Home.jsx";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <html>
-    <div className={styles.main}>
+    <div className={styles.container}>
       <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#90cdf4" />
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
 
-        
-        <title>Codex Reader</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript">var submitted=false;</script>
-        {
-        /* <meta httpEquiv="Content-Security-Policy" 
-        content="frame-src atlassian-companion:;"></meta> */}
       </Head>
-      <body className={styles.body}>
-        <Home2 />
-      </body>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
-    </html>
-  );
+  )
 }
